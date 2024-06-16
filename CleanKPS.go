7b6fc20d4bf9ba84c753bf3c912af75c72ae5516
Yan Abaev <yanabaevlxst@gmail.com>
2024-06-17 00:41:18 +0300

    мелкие изменения и добавление цикла

--- CleanKPS.go
+++ CleanKPS.go
@@ -11,50 +11,57 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Простой консольный калькулятор")
+	fmt.Println("Проект для Академии")
 	fmt.Println("Введите выражение (например, 2 + 3):")
 
-	receiving_values, _ := reader.ReadString('\n')
-	receiving_values = strings.TrimSpace(receiving_values)
+	for {
+		receiving_values, _ := reader.ReadString('\n')
+		receiving_values = strings.TrimSpace(receiving_values)
 
-	parts := strings.Split(receiving_values, " ")
-	if len(parts) != 3 {
-		fmt.Println("Неверный формат ввода. Пример: 2 + 3")
-		return
-	}
-
-	first_num, panic := strconv.ParseFloat(parts[0], 64)
-	if panic != nil {
-		fmt.Println("Ошибка преобразования первого числа:", panic)
-		return
-	}
-
-	second_num, panic := strconv.ParseFloat(parts[2], 64)
-	if panic != nil {
-		fmt.Println("Ошибка преобразования второго числа:", panic)
-		return
-	}
-
-	math_oper := parts[1]
-
-	var answer float64
-	switch math_oper {
-	case "+":
-		answer = first_num + second_num
-	case "-":
-		answer = first_num - second_num
-	case "*":
-		answer = first_num * second_num
-	case "/":
-		if second_num == 0 {
-			fmt.Println("Деление на ноль невозможно")
-			return
+		if receiving_values == "exit" {
+			fmt.Println("Вы вышли из калькулятора.")
+			break
 		}
-		answer = first_num / second_num
-	default:
-		fmt.Println("Неподдерживаемый оператор:", math_oper)
-		return
-	}
 
-	fmt.Printf("Результат: %.2f\n", answer)
+		parts := strings.Split(receiving_values, " ")
+		if len(parts) != 3 {
+			fmt.Println("Неверный формат ввода. Пример: 2 + 3")
+			continue
+		}
+
+		first_num, err := strconv.ParseFloat(parts[0], 64)
+		if err != nil {
+			fmt.Println("Первое число не было преобразовано:", err)
+			continue
+		}
+
+		second_num, err := strconv.ParseFloat(parts[2], 64)
+		if err != nil {
+			fmt.Println("Второе число не было преобразовано:", err)
+			continue
+		}
+
+		math_oper := parts[1]
+
+		var answer float64
+		switch math_oper {
+		case "+":
+			answer = first_num + second_num
+		case "-":
+			answer = first_num - second_num
+		case "*":
+			answer = first_num * second_num
+		case "/":
+			if second_num == 0 {
+				fmt.Println("Деление на ноль не пройдет!")
+				continue
+			}
+			answer = first_num / second_num
+		default:
+			fmt.Println("Оператор не понятный:", math_oper)
+			continue
+		}
+
+		fmt.Printf("Ответ: %.2f\n", answer)
+	}
 }
