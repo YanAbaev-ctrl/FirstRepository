f95658f0a54767f3fba310ddcb0460e79cd20a06
Yan Abaev <yanabaevlxst@gmail.com>
2024-06-18 23:24:28 +0300

    полностью переписана логика для интеграции римской нотации

--- CleanKPS.go
+++ CleanKPS.go
@@ -8,136 +8,125 @@ import (
 	"strings"
 )
 
-var rim_arab = map[string]int{
-	"I": 1, "IV": 4, "V": 5, "IX": 9,
-	"X": 10, "XL": 40, "L": 50, "XC": 90,
-	"C": 100, "CD": 400, "D": 500, "CM": 900,
-	"M": 1000,
+var rimtoarab = map[string]int{
+	"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6,
+	"VII": 7, "VIII": 8, "IX": 9, "X": 10,
 }
 
-var arab_rim = map[int]string{
-	1: "I", 4: "IV", 5: "V", 9: "IX",
-	10: "X", 40: "XL", 50: "L", 90: "XC",
-	100: "C", 400: "CD", 500: "D", 900: "CM",
-	1000: "M",
-}
-
-func rimtoarab(rimn string) (int, error) {
-
-	var result int
-	rimsd := 0
-
-	for i := len(rimn) - 1; i >= 0; i-- {
-		value := rim_arab[string(rimn[i])]
-		if value == 0 {
-			return 0, fmt.Errorf("Неверное римское число: %s", rimn)
-		}
-		if value < rimsd {
-			result -= value
-		} else {
-			result += value
-		}
-
-		result = value
-	}
-	return result, nil
-
-}
-
-func arabtorim(arabn int) (string, error) {
-	if arabn <= 0 || arabn > 3999 {
-		return "", fmt.Errorf("Это число нельзя конвертировать в римское: %d", arabn)
-	}
-	var result strings.Builder
-	for arabn > 0 {
-		for arabval, rimsym := range arab_rim {
-			if arabn >= arabval {
-				result.WriteString(rimsym)
-				arabn -= arabval
-				break
-			}
-		}
-	}
-	return result.String(), nil
+var arabtorim = []string{
+	"", "I", "II", "III", "IV", "V", "VI",
+	"VII", "VIII", "IX", "X", "XI", "XII",
+	"XIII", "XIV", "XV", "XVI", "XVII", "XVIII",
+	"XIX", "XX",
 }
 
 func main() {
-
 	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Введите выражение: ")
+	receiving_values, _ := reader.ReadString('\n')
+	receiving_values = strings.TrimSpace(receiving_values)
 
-	fmt.Println("Проект для Академии")
-	fmt.Println("Введите выражение (например, 2 + 3):")
-
-	for {
-		receiving_values, _ := reader.ReadString('\n')
-		receiving_values = strings.TrimSpace(receiving_values)
-
-		if receiving_values == "exit" {
-			fmt.Println("Вы вышли из калькулятора.")
-			break
-		}
-
-		parts := strings.Split(receiving_values, " ")
-		if len(parts) != 3 {
-			fmt.Println("Неверный формат ввода. Пример: 2 + 3")
-			continue
-		}
-
-		first_num, err := strconv.Atoi(parts[0])
-		if err != nil {
-			fmt.Println("Первое число не было преобразовано:", err)
-			break
-		}
-
-		second_num, err := strconv.Atoi(parts[2])
-		if err != nil {
-			fmt.Println("Второе число не было преобразовано:", err)
-			break
-		}
-
-		if first_num >= 1 && first_num <= 10 && second_num >= 1 && second_num <= 10 {
-
-			math_oper := parts[1]
-
-			var answer int
-			switch math_oper {
-			case "+":
-				answer = first_num + second_num
-
-			case "-":
-				answer = first_num - second_num
-
-			case "*":
-				answer = first_num * second_num
-
-			case "/":
-
-				if second_num == 0 {
-					fmt.Println("Делить на ноль нельзя!")
-					continue
-				}
-
-				answer = first_num / second_num
-
-			default:
-				fmt.Println("Оператор не понятный:", math_oper)
-				os.Exit(1)
-			}
-
-			if answer > 0 {
-				fmt.Println("Результат является положительным числом.")
-				break
-			} else if answer == 0 {
-				fmt.Println(answer)
-				continue
-			}
-
-			fmt.Printf("Ответ: %.d\n", answer)
-
-		} else {
-			fmt.Println("Введенное Вами число не входит в диапазон [1;10]")
-			break
-		}
-
+	if receiving_values == "exit" {
+		fmt.Println("Вы вышли из калькулятора.")
+		os.Exit(0)
 	}
+
+	answer, err := calculate(receiving_values)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
+
+	fmt.Println("Результат:", answer)
+
+}
+
+func calculate(enter string) (string, error) {
+	parts := strings.Fields(enter)
+	if len(parts) != 3 {
+		return "", fmt.Errorf("неправильный формат ввода")
+	}
+
+	first_num, operator, second_num := parts[0], parts[1], parts[2]
+
+	// Определяем тип введенных данных (арабские или римские числа)
+	rimfirstnum := isRomanNumeral(first_num)
+	rimsecondnum := isRomanNumeral(second_num)
+
+	if rimfirstnum != rimsecondnum {
+		return "", fmt.Errorf("нельзя смешивать римские и арабские числа")
+	}
+
+	// Парсим операнды в соответствии с их типом
+	var a, b int
+	var err error
+	if rimfirstnum {
+		a, b, err = parseRomanOperands(first_num, second_num)
+	} else {
+		a, b, err = parseOperands(first_num, second_num)
+	}
+	if err != nil {
+		return "", err
+	}
+
+	var result int
+	switch operator {
+	case "+":
+		result = a + b
+	case "-":
+		result = a - b
+	case "*":
+		result = a * b
+	case "/":
+		if b == 0 {
+			return "", fmt.Errorf("деление на ноль")
+		}
+		result = a / b
+	default:
+		return "", fmt.Errorf("неизвестная операция")
+	}
+
+	// Возвращаем результат в соответствующем формате
+	if rimfirstnum {
+		if result <= 0 {
+			return "", fmt.Errorf("римские числа не могут быть отрицательными или равными нулю")
+		}
+		return arabtorim[result], nil
+	}
+	return strconv.Itoa(result), nil
+}
+
+func parseOperands(first_num, second_num string) (int, int, error) {
+	a, err := strconv.Atoi(first_num)
+	if err != nil {
+		return 0, 0, err
+	}
+	b, err := strconv.Atoi(second_num)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if a < 1 || a > 10 || b < 1 || b > 10 {
+		return 0, 0, fmt.Errorf("числа должны быть в диапазоне от 1 до 10 включительно")
+	}
+
+	return a, b, nil
+}
+
+func parseRomanOperands(first_num, second_num string) (int, int, error) {
+	a, check := rimtoarab[first_num]
+	if !check {
+		return 0, 0, fmt.Errorf("неверное римское число: %s", first_num)
+	}
+	b, check := rimtoarab[second_num]
+	if !check {
+		return 0, 0, fmt.Errorf("неверное римское число: %s", second_num)
+	}
+
+	return a, b, nil
+}
+
+func isRomanNumeral(enter string) bool {
+	_, check := rimtoarab[enter]
+	return check
 }
